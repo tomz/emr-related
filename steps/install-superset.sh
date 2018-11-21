f69ee50a0a2b246bf1552750cdf079ec13abfd0e
Tom Zeng <tomzeng@amazon.com>
2018-11-20 20:44:02 -0800

    Update the code that sets the default Hive/Presto/Spark data sources to work the latest version of superset

--- steps/install-superset.sh
+++ steps/install-superset.sh
@@ -5,6 +5,7 @@ set -x -e
 # for installing Superset on AWS EMR 5+
 #
 # 2018-03-08 - Tom Zeng tomzeng@amazon.com, initial version
+# 2018-11-10 - Tom Zeng tomzeng@amazon.com, updated for the latest superset
 
 # check for master node
 IS_MASTER=false
@@ -60,13 +61,13 @@ done
 if [ "$IS_MASTER" = true ]; then
 
 export PATH=/usr/local/bin:$PATH
-sudo python -m pip install --upgrade pip
-sudo mv /usr/bin/pip /usr/bin/pip-save || true
-sudo ln -s /usr/local/bin/pip /usr/bin/ || true
-sudo python -m pip install --upgrade setuptools
-sudo yum install -y gcc gcc-c++ libffi-devel python-devel python-pip python-wheel openssl-devel libsasl2-devel openldap-devel
+sudo yum install -y gcc gcc-c++ libffi-devel python36 python36-devel python36-pip python-wheel openssl-devel libsasl2-devel openldap-devel
+sudo python3 -m pip install --upgrade pip
+#sudo mv /usr/bin/pip /usr/bin/pip-save || true
+#sudo ln -s /usr/local/bin/pip /usr/bin/ || true
+sudo python3 -m pip install --upgrade setuptools
   
-sudo python -m pip install superset
+sudo python3 -m pip install superset --ignore-installed colorama
 
 fabmanager create-admin --app superset --username $SUPERSET_USERNAME --password $SUPERSET_PASSWORD --firstname $SUPERSET_FIRSTNAME --lastname $SUPERSET_LASTNAME --email $SUPERSET_EMAIL || true
 #TODO - add support for MySQL/Postgres in addition to SQLite
@@ -76,9 +77,30 @@ if [ "$SUPERSET_LOAD_EXAMPLES" = true ]; then
 fi
 superset init
 cd ~/.superset
-sqlite3 superset.db "INSERT INTO dbs (created_on,changed_on,id,database_name,sqlalchemy_uri,created_by_fk,changed_by_fk,password,cache_timeout,extra,select_as_create_table_as,allow_ctas,expose_in_sqllab,force_ctas_schema,allow_run_async,allow_run_sync,allow_dml,perm,verbose_name,impersonate_user,allow_multi_schema_metadata_fetch) VALUES(datetime('now'),datetime('now'),2,'Hive','hive://localhost:10000',1,1,NULL,NULL,'{\"metadata_params\": {},\"engine_params\": {}}',0,1,1,'',0,1,1,'[Hive].(id:2)',NULL,1,1);"
-sqlite3 superset.db "INSERT INTO dbs (created_on,changed_on,id,database_name,sqlalchemy_uri,created_by_fk,changed_by_fk,password,cache_timeout,extra,select_as_create_table_as,allow_ctas,expose_in_sqllab,force_ctas_schema,allow_run_async,allow_run_sync,allow_dml,perm,verbose_name,impersonate_user,allow_multi_schema_metadata_fetch) VALUES(datetime('now'),datetime('now'),3,'Spark','hive://localhost:10001',1,1,NULL,NULL,'{\"metadata_params\": {},\"engine_params\": {}}',0,1,1,'',0,1,1,'[Spark].(id:3)',NULL,1,1);"
-sqlite3 superset.db "INSERT INTO dbs (created_on,changed_on,id,database_name,sqlalchemy_uri,created_by_fk,changed_by_fk,password,cache_timeout,extra,select_as_create_table_as,allow_ctas,expose_in_sqllab,force_ctas_schema,allow_run_async,allow_run_sync,allow_dml,perm,verbose_name,impersonate_user,allow_multi_schema_metadata_fetch) VALUES(datetime('now'),datetime('now'),4,'Presto','presto://localhost:8889/hive/default',1,1,NULL,NULL,'{\"metadata_params\": {},\"engine_params\": {}}',0,1,1,'',0,1,1,'[Presto].(id:4)',NULL,1,1);"
+sqlite3 superset.db <<SQL
+INSERT INTO "dbs" VALUES(datetime('now'),datetime('now'),2,'Hive','hive://localhost:10000',1,1,NULL,NULL,'{
+    "metadata_params": {},
+    "engine_params": {},
+    "metadata_cache_timeout": {},
+    "schemas_allowed_for_csv_upload": []
+}
+',0,1,1,'',0,1,1,'[Hive].(id:2)',NULL,1,1,1);
+INSERT INTO "dbs" VALUES(datetime('now'),datetime('now'),3,'Spark','hive://localhost:10001',1,1,NULL,NULL,'{
+    "metadata_params": {},
+    "engine_params": {},
+    "metadata_cache_timeout": {},
+    "schemas_allowed_for_csv_upload": []
+}
+',0,1,1,'',0,1,1,'[Spark].(id:3)',NULL,1,1,1);
+INSERT INTO "dbs" VALUES(datetime('now'),datetime('now'),4,'Presto','presto://localhost:8889/hive/default',1,1,NULL,NULL,'{
+    "metadata_params": {},
+    "engine_params": {},
+    "metadata_cache_timeout": {},
+    "schemas_allowed_for_csv_upload": []
+}
+',0,1,1,'',0,1,1,'[Presto].(id:4)',NULL,1,1,1);
+SQL
+
 sudo -u spark /usr/lib/spark/sbin/start-thriftserver.sh &
 #TODO - replace the following with setting up and running superset as a service 
 superset runserver -p ${SUPERSET_PORT} &
